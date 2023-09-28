8fb2c2e2de0cf531afcb0c6a954f006a9fe69b39
Muhimbura Brian Mart <brianmigel@gmail.com>
2023-09-28 13:20:05 +0300

    Add logs to prequalification

--- fineract-provider/src/main/java/org/apache/fineract/organisation/prequalification/service/BureauValidationWritePlatformServiceImpl.java
+++ fineract-provider/src/main/java/org/apache/fineract/organisation/prequalification/service/BureauValidationWritePlatformServiceImpl.java
@@ -24,12 +24,15 @@ import org.apache.fineract.infrastructure.core.data.CommandProcessingResult;
 import org.apache.fineract.infrastructure.core.data.CommandProcessingResultBuilder;
 import org.apache.fineract.infrastructure.security.service.PlatformSecurityContext;
 import org.apache.fineract.organisation.prequalification.domain.PreQualificationMemberRepository;
+import org.apache.fineract.organisation.prequalification.domain.PreQualificationStatusLogRepository;
 import org.apache.fineract.organisation.prequalification.domain.PrequalificationGroup;
 import org.apache.fineract.organisation.prequalification.domain.PrequalificationGroupMember;
 import org.apache.fineract.organisation.prequalification.domain.PrequalificationGroupRepositoryWrapper;
 import org.apache.fineract.organisation.prequalification.domain.PrequalificationMemberIndication;
 import org.apache.fineract.organisation.prequalification.domain.PrequalificationStatus;
+import org.apache.fineract.organisation.prequalification.domain.PrequalificationStatusLog;
 import org.apache.fineract.organisation.prequalification.domain.ValidationChecklistResultRepository;
+import org.apache.fineract.useradministration.domain.AppUser;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -42,6 +45,7 @@ public class BureauValidationWritePlatformServiceImpl implements BureauValidatio
     private final PlatformSecurityContext context;
     private final PrequalificationGroupRepositoryWrapper prequalificationGroupRepositoryWrapper;
     private final PreQualificationMemberRepository preQualificationMemberRepository;
+    private final PreQualificationStatusLogRepository preQualificationStatusLogRepository;
     private final ValidationChecklistResultRepository validationChecklistResultRepository;
     private final PlatformSecurityContext platformSecurityContext;
     private final JdbcTemplate jdbcTemplate;
@@ -49,6 +53,7 @@ public class BureauValidationWritePlatformServiceImpl implements BureauValidatio
     public BureauValidationWritePlatformServiceImpl(PlatformSecurityContext context,
             final PreQualificationMemberRepository preQualificationMemberRepository,
             PrequalificationGroupRepositoryWrapper prequalificationGroupRepositoryWrapper,
+            final PreQualificationStatusLogRepository preQualificationStatusLogRepository,
             ValidationChecklistResultRepository validationChecklistResultRepository, PlatformSecurityContext platformSecurityContext,
             JdbcTemplate jdbcTemplate) {
         this.context = context;
@@ -56,6 +61,7 @@ public class BureauValidationWritePlatformServiceImpl implements BureauValidatio
         this.validationChecklistResultRepository = validationChecklistResultRepository;
         this.platformSecurityContext = platformSecurityContext;
         this.preQualificationMemberRepository = preQualificationMemberRepository;
+        this.preQualificationStatusLogRepository = preQualificationStatusLogRepository;
         this.jdbcTemplate = jdbcTemplate;
     }
 
@@ -64,6 +70,9 @@ public class BureauValidationWritePlatformServiceImpl implements BureauValidatio
 
         PrequalificationGroup prequalificationGroup = this.prequalificationGroupRepositoryWrapper
                 .findOneWithNotFoundDetection(prequalificationId);
+
+        Integer fromStatus = prequalificationGroup.getStatus();
+        AppUser addedBy = this.context.getAuthenticatedUserIfPresent();
 
         // TODO --PROCESS THE PREQUALIFICATION GROUP WITH THE BUREAU AND UPDATE MEMBERS WITH THE RESULTS
         List<PrequalificationGroupMember> members = this.preQualificationMemberRepository
@@ -76,6 +85,11 @@ public class BureauValidationWritePlatformServiceImpl implements BureauValidatio
         }
         prequalificationGroup.updateStatus(PrequalificationStatus.BURO_CHECKED);
         this.prequalificationGroupRepositoryWrapper.save(prequalificationGroup);
+
+        PrequalificationStatusLog statusLog = PrequalificationStatusLog.fromJson(addedBy, fromStatus,
+                prequalificationGroup.getStatus(), null, prequalificationGroup);
+
+        this.preQualificationStatusLogRepository.saveAndFlush(statusLog);
 
         return new CommandProcessingResultBuilder() //
                 .withCommandId(command.commandId()) //
