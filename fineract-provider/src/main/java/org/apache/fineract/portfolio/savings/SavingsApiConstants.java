c51907c79c5ea2235e6393feabde09104dc48abc
Moses Kalema <mkgremo20@gmail.com>
2023-11-16 12:22:01 -0600

    Fbr 401 and 402 create default gurantee savings account for client (#277)
    * FBR-401 added script to create savings gurantee product
    
    * FBR-402 create default gurantee savings account when activating client

--- fineract-provider/src/main/java/org/apache/fineract/portfolio/savings/SavingsApiConstants.java
+++ fineract-provider/src/main/java/org/apache/fineract/portfolio/savings/SavingsApiConstants.java
@@ -143,6 +143,7 @@ public class SavingsApiConstants {
     public static final String savingsAmountOnHold = "savingsAmountOnHold";
     public static final String withHoldTaxParamName = "withHoldTax";
     public static final String taxGroupIdParamName = "taxGroupId";
+    public static final String GURANTEE_PRODUCT_NAME = "Garantías";
 
     // transaction parameters
     public static final String transactionDateParamName = "transactionDate";
