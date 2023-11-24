c9b239a1b6bd3f2e791e1bf834dd0d685105135f
Muhimbura Brian Mart <brianmigel@gmail.com>
2023-11-24 19:29:26 +0300

    Transfer group validations

--- fineract-provider/src/main/java/org/apache/fineract/portfolio/group/data/GroupGeneralData.java
+++ fineract-provider/src/main/java/org/apache/fineract/portfolio/group/data/GroupGeneralData.java
@@ -264,7 +264,7 @@ public class GroupGeneralData implements Serializable {
         final Collection<CodeValueData> closureReasons = null;
         return new GroupGeneralData(groupId, accountNo, groupName, null, null, null, null, null, null, null, null, null, null, null,
                 clientMembers, null, null, null, null, null, groupRoles, null, null, null, null, closureReasons, null, null, null, null,
-                null, null, null, null);
+                null, null, null, null, null, null);
     }
 
     public static GroupGeneralData template(final Long officeId, final Long centerId, final String accountNo, final String centerName,
@@ -281,7 +281,7 @@ public class GroupGeneralData implements Serializable {
         return new GroupGeneralData(null, accountNo, null, null, null, null, officeId, null, centerId, centerName, staffId, staffName, null,
                 null, clientMembers, null, centerOptions, officeOptions, staffOptions, clientOptions, groupRoles, availableRoles, null,
                 null, null, closureReasons, null, parentOfficesOptions, responsibleUserOptions, portfolioCenterOptions,
-                centerGroupLocations, null, null, null);
+                centerGroupLocations, null, null, null, null, null);
     }
 
     public static GroupGeneralData withTemplate(final GroupGeneralData templatedGrouping, final GroupGeneralData grouping) {
@@ -293,7 +293,7 @@ public class GroupGeneralData implements Serializable {
                 grouping.calendarsData, grouping.collectionMeetingCalendar, grouping.closureReasons, templatedGrouping.timeline,
                 templatedGrouping.parentOfficesOptions, templatedGrouping.responsibleUserOptions, templatedGrouping.portfolioCenterOptions,
                 templatedGrouping.centerGroupLocations, grouping.prequalificationNumber, grouping.prequalificationId,
-                grouping.prequalificationStatus);
+                grouping.prequalificationStatus, grouping.meetingStartTime, grouping.meetingEndTime);
 
         return mapDTO(grouping, ret);
     }
@@ -335,15 +335,15 @@ public class GroupGeneralData implements Serializable {
                 grouping.selectedRole, calendarsData, collectionMeetingCalendar, grouping.closureReasons, grouping.timeline,
                 grouping.parentOfficesOptions, grouping.responsibleUserOptions, grouping.portfolioCenterOptions,
                 grouping.centerGroupLocations, grouping.prequalificationNumber, grouping.prequalificationId,
-                grouping.prequalificationStatus);
+                grouping.prequalificationStatus, grouping.meetingStartTime, grouping.meetingEndTime);
 
         return mapDTO(grouping, ret);
     }
 
     public static GroupGeneralData instance(final Long id, final String accountNo, final String name, final String externalId,
-            final EnumOptionData status, final LocalDate activationDate, final Long officeId, final String officeName, final Long centerId,
-            final String centerName, final Long staffId, final String staffName, final String hierarchy, final String groupLevel,
-            final GroupTimelineData timeline) {
+                                            final EnumOptionData status, final LocalDate activationDate, final Long officeId, final String officeName, final Long centerId,
+                                            final String centerName, final Long staffId, final String staffName, final String hierarchy, final String groupLevel,
+                                            final GroupTimelineData timeline, LocalTime meetingStartTime, LocalTime meetingEndTime) {
 
         final Collection<ClientData> clientMembers = null;
         final Collection<ClientData> activeClientMembers = null;
@@ -365,21 +365,22 @@ public class GroupGeneralData implements Serializable {
         return new GroupGeneralData(id, accountNo, name, externalId, status, activationDate, officeId, officeName, centerId, centerName,
                 staffId, staffName, hierarchy, groupLevel, clientMembers, activeClientMembers, centerOptions, officeOptions, staffOptions,
                 clientOptions, groupRoles, availableRoles, role, calendarsData, collectionMeetingCalendar, closureReasons, timeline,
-                parentOfficesOptions, responsibleUserOptions, portfolioCenterOptions, centerGroupLocations, null, null, null);
+                parentOfficesOptions, responsibleUserOptions, portfolioCenterOptions, centerGroupLocations,
+                null, null, null, meetingStartTime, meetingEndTime);
     }
 
     private GroupGeneralData(final Long id, final String accountNo, final String name, final String externalId, final EnumOptionData status,
-            final LocalDate activationDate, final Long officeId, final String officeName, final Long centerId, final String centerName,
-            final Long staffId, final String staffName, final String hierarchy, final String groupLevel,
-            final Collection<ClientData> clientMembers, final Collection<ClientData> activeClientMembers,
-            final Collection<CenterData> centerOptions, final Collection<OfficeData> officeOptions,
-            final Collection<StaffData> staffOptions, final Collection<ClientData> clientOptions,
-            final Collection<GroupRoleData> groupRoles, final Collection<CodeValueData> availableRoles, final GroupRoleData role,
-            final Collection<CalendarData> calendarsData, final CalendarData collectionMeetingCalendar,
-            final Collection<CodeValueData> closureReasons, final GroupTimelineData timeline,
-            final Collection<OfficeData> parentOfficesOptions, final Collection<AppUserData> responsibleUserOptions,
-            final Collection<PortfolioCenterData> portfolioCenterOptions, Collection<EnumOptionData> centerGroupLocations,
-            String prequalificationNumber, Long prequalificationId, EnumOptionData prequalificationStatus) {
+                             final LocalDate activationDate, final Long officeId, final String officeName, final Long centerId, final String centerName,
+                             final Long staffId, final String staffName, final String hierarchy, final String groupLevel,
+                             final Collection<ClientData> clientMembers, final Collection<ClientData> activeClientMembers,
+                             final Collection<CenterData> centerOptions, final Collection<OfficeData> officeOptions,
+                             final Collection<StaffData> staffOptions, final Collection<ClientData> clientOptions,
+                             final Collection<GroupRoleData> groupRoles, final Collection<CodeValueData> availableRoles, final GroupRoleData role,
+                             final Collection<CalendarData> calendarsData, final CalendarData collectionMeetingCalendar,
+                             final Collection<CodeValueData> closureReasons, final GroupTimelineData timeline,
+                             final Collection<OfficeData> parentOfficesOptions, final Collection<AppUserData> responsibleUserOptions,
+                             final Collection<PortfolioCenterData> portfolioCenterOptions, Collection<EnumOptionData> centerGroupLocations,
+                             String prequalificationNumber, Long prequalificationId, EnumOptionData prequalificationStatus, LocalTime meetingStartTime, LocalTime meetingEndTime) {
         this.id = id;
         this.accountNo = accountNo;
         this.name = name;
@@ -428,6 +429,8 @@ public class GroupGeneralData implements Serializable {
         this.prequalificationNumber = prequalificationNumber;
         this.prequalificationStatus = prequalificationStatus;
         this.prequalificationId = prequalificationId;
+        this.meetingStartTime = meetingStartTime;
+        this.meetingEndTime = meetingEndTime;
     }
 
     public Long getId() {
@@ -465,7 +468,7 @@ public class GroupGeneralData implements Serializable {
                 grouping.centerOptions, grouping.officeOptions, grouping.staffOptions, grouping.clientOptions, grouping.groupRoles,
                 grouping.availableRoles, selectedRole, grouping.calendarsData, grouping.collectionMeetingCalendar, grouping.closureReasons,
                 null, null, null, null, null, grouping.prequalificationNumber, grouping.prequalificationId,
-                grouping.prequalificationStatus);
+                grouping.prequalificationStatus, grouping.meetingStartTime, grouping.meetingEndTime);
     }
 
     public static GroupGeneralData withClosureReasons(final Collection<CodeValueData> closureReasons) {
@@ -502,7 +505,8 @@ public class GroupGeneralData implements Serializable {
         return new GroupGeneralData(id, accountNo, name, externalId, status, activationDate, officeId, officeName, centerId, centerName,
                 staffId, staffName, hierarchy, groupLevel, clientMembers, activeClientMembers, centerOptions, officeOptions, staffOptions,
                 clientOptions, groupRoles, availableRoles, role, calendarsData, collectionMeetingCalendar, closureReasons, null,
-                parentOfficesOptions, responsibleUserOptions, portfolioCenterOptions, centerGroupLocations, null, null, null);
+                parentOfficesOptions, responsibleUserOptions, portfolioCenterOptions, centerGroupLocations,
+                null, null, null, null, null);
     }
 
     public Collection<ClientData> clientMembers() {
