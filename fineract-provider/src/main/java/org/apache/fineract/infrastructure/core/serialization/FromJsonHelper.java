bdd773279a91a12f4f811249c529efbce7638a98
Faheem Ahmad <faheem@fiter.io>
2023-12-15 01:21:46 +0500

    FBR-430 Date and BigDecimal locale issue for Spanish fixed

--- fineract-provider/src/main/java/org/apache/fineract/infrastructure/core/serialization/FromJsonHelper.java
+++ fineract-provider/src/main/java/org/apache/fineract/infrastructure/core/serialization/FromJsonHelper.java
@@ -246,7 +246,15 @@ public class FromJsonHelper {
     }
 
     public BigDecimal extractBigDecimalWithLocaleNamed(final String parameterName, final JsonElement element) {
-        return this.helperDelegator.extractBigDecimalWithLocaleNamed(parameterName, element, new HashSet<String>());
+        BigDecimal withLocale =  this.helperDelegator.extractBigDecimalWithLocaleNamed(parameterName, element, new HashSet<String>());
+        //FBR-430 2024.00 is converted as 202400 in Spanish locale. Below fix is to get correct value if locale is Spanish
+        if (withLocale != null && this.helperDelegator.extractLocaleValueAsString(element.getAsJsonObject()).equalsIgnoreCase("es") ) {
+            BigDecimal withoutLocale = this.extractBigDecimalNamed(parameterName, element, new HashSet<String>());
+            if (withLocale.compareTo(withoutLocale) != 0) {
+                return withoutLocale;
+            }
+        }
+        return withLocale;
     }
 
     public BigDecimal extractBigDecimalWithLocaleNamed(final String parameterName, final JsonElement element,
