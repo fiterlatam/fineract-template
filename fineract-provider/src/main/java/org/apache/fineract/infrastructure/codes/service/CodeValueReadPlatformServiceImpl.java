f0d8a736206f15ea1309bc938176f82a937a990e
fiter-julius-oketayot <140818859+fiter-julius-oketayot@users.noreply.github.com>
2024-03-15 17:53:41 +0300

    Revert "feature/SU-47-01 - Campos creación Aliados" (#558)

--- fineract-provider/src/main/java/org/apache/fineract/infrastructure/codes/service/CodeValueReadPlatformServiceImpl.java
+++ fineract-provider/src/main/java/org/apache/fineract/infrastructure/codes/service/CodeValueReadPlatformServiceImpl.java
@@ -26,18 +26,16 @@ import org.apache.fineract.infrastructure.codes.exception.CodeValueNotFoundExcep
 import org.apache.fineract.infrastructure.security.service.PlatformSecurityContext;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.Cacheable;
-import org.springframework.context.annotation.Primary;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Service;
 
 @Service
-@Primary
 public class CodeValueReadPlatformServiceImpl implements CodeValueReadPlatformService {
 
-    protected final JdbcTemplate jdbcTemplate;
-    protected final PlatformSecurityContext context;
+    private final JdbcTemplate jdbcTemplate;
+    private final PlatformSecurityContext context;
 
     @Autowired
     public CodeValueReadPlatformServiceImpl(final PlatformSecurityContext context, final JdbcTemplate jdbcTemplate) {
@@ -73,7 +71,7 @@ public class CodeValueReadPlatformServiceImpl implements CodeValueReadPlatformSe
         this.context.authenticatedUser();
 
         final CodeValueDataMapper rm = new CodeValueDataMapper();
-        final String sql = "select " + rm.schema() + "where c.code_name like ? and cv.is_active = true and cv.parent_id IS NULL order by position";
+        final String sql = "select " + rm.schema() + "where c.code_name like ? and cv.is_active = true order by position";
 
         return this.jdbcTemplate.query(sql, rm, new Object[] { code }); // NOSONAR
     }
