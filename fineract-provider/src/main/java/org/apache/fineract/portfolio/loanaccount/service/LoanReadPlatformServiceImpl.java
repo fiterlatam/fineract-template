aeb9f52b3640ce670e23d61ec9477401bfe0be6b
Muhimbura Brian Mart <brianmigel@gmail.com>
2024-05-06 14:24:24 +0300

    merge qa fbr 645

--- fineract-provider/src/main/java/org/apache/fineract/portfolio/loanaccount/service/LoanReadPlatformServiceImpl.java
+++ fineract-provider/src/main/java/org/apache/fineract/portfolio/loanaccount/service/LoanReadPlatformServiceImpl.java
@@ -350,6 +350,7 @@ public class LoanReadPlatformServiceImpl implements LoanReadPlatformService {
 
         final AppUser currentUser = this.context.authenticatedUser();
         final String hierarchy = currentUser.getOffice().getHierarchy();
+        final String hierarchySearchString = hierarchy + "%";
 
         final StringBuilder sqlBuilder = new StringBuilder(200);
         sqlBuilder.append("select " + sqlGenerator.calcFoundRows() + " ");
@@ -362,8 +363,7 @@ public class LoanReadPlatformServiceImpl implements LoanReadPlatformService {
         // but that at present is an edge case
         sqlBuilder.append(" join m_office o on (o.id = c.office_id or o.id = g.office_id) ");
         sqlBuilder.append(" left join m_office transferToOffice on transferToOffice.id = c.transfer_to_office_id ");
-        sqlBuilder.append(" where (o.hierarchy LIKE CONCAT(?, '%') OR ? like CONCAT(o.hierarchy, '%') " +
-                "OR transferToOffice.hierarchy LIKE CONCAT(?, '%') OR ? like CONCAT(transferToOffice.hierarchy, '%'))");
+        sqlBuilder.append(" where ( o.hierarchy like ? or transferToOffice.hierarchy like ?)");
 
         final Collection<AgencyData> agencyOptions = this.agencyReadPlatformService.retrieveAllByUser();
         final Set<Long> agencyIds = agencyOptions.stream().map(AgencyData::getId).collect(Collectors.toSet());
@@ -372,12 +372,12 @@ public class LoanReadPlatformServiceImpl implements LoanReadPlatformService {
             sqlBuilder.append(" AND agency.id IN ( ").append(agencyIdParams).append(") ");
         }
 
-        int arrayPos = 4;
+        int arrayPos = 2;
         List<Object> extraCriterias = new ArrayList<>();
-        extraCriterias.add(hierarchy);
-        extraCriterias.add(hierarchy);
-        extraCriterias.add(hierarchy);
-        extraCriterias.add(hierarchy);
+        extraCriterias.add(hierarchySearchString);
+        extraCriterias.add(hierarchySearchString);
+//        extraCriterias.add(hierarchy);
+//        extraCriterias.add(hierarchy);
 
         if (searchParameters != null) {
             final LocalDate disbursementStartDate = searchParameters.getDisbursementStartDate();
@@ -827,7 +827,8 @@ public class LoanReadPlatformServiceImpl implements LoanReadPlatformService {
                     + " left join m_loan_recalculation_details lir on lir.loan_id = l.id " + " join m_currency rc on rc."
                     + sqlGenerator.escape("code") + " = l.currency_code" //
                     + " left join m_client c on c.id = l.client_id" //
-                    + " left join m_group g on g.id = l.group_id" //
+                    + " left join m_group_client gcl on gcl.client_id = c.id" //
+                    + " left join m_group g on g.id = gcl.group_id" //
                     + " left join m_group center on center.id = g.parent_id" //
                     + " left join m_portfolio portfolio on portfolio.id = center.portfolio_id" //
                     + " left join m_supervision supv on supv.id = portfolio.supervision_id" //
