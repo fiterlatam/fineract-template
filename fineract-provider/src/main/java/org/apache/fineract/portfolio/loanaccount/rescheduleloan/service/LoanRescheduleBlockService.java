fd7fecee49f98e67df871ee84ee94bedcb303332
Napho <starnapho@gmail.com>
2024-05-21 10:03:26 +0300

    SU-40 Add restructurado blocking

---
+++ fineract-provider/src/main/java/org/apache/fineract/portfolio/loanaccount/rescheduleloan/service/LoanRescheduleBlockService.java
@@ -0,0 +1,3 @@
+package org.apache.fineract.portfolio.loanaccount.rescheduleloan.service;
+
+public interface LoanRescheduleBlockService {}
---
+++ fineract-provider/src/main/java/org/apache/fineract/portfolio/loanaccount/rescheduleloan/service/LoanRescheduleBlockServiceImpl.java
@@ -0,0 +1,54 @@
+package org.apache.fineract.portfolio.loanaccount.rescheduleloan.service;
+
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import org.apache.fineract.infrastructure.clientblockingreasons.domain.BlockLevel;
+import org.apache.fineract.infrastructure.clientblockingreasons.domain.BlockingReasonSetting;
+import org.apache.fineract.infrastructure.clientblockingreasons.domain.BlockingReasonSettingEnum;
+import org.apache.fineract.infrastructure.clientblockingreasons.domain.BlockingReasonSettingsRepositoryWrapper;
+import org.apache.fineract.infrastructure.codes.domain.CodeValue;
+import org.apache.fineract.infrastructure.core.service.DateUtils;
+import org.apache.fineract.infrastructure.event.business.BusinessEventListener;
+import org.apache.fineract.infrastructure.event.business.domain.loan.LoanRescheduledDueAdjustScheduleBusinessEvent;
+import org.apache.fineract.infrastructure.event.business.service.BusinessEventNotifierService;
+import org.apache.fineract.portfolio.loanaccount.domain.Loan;
+import org.apache.fineract.portfolio.loanaccount.service.LoanBlockWritePlatformService;
+import org.apache.fineract.portfolio.loanproduct.domain.LoanProductType;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class LoanRescheduleBlockServiceImpl implements LoanRescheduleBlockService {
+
+    private final BusinessEventNotifierService businessEventNotifierService;
+    private final BlockingReasonSettingsRepositoryWrapper loanBlockingReasonRepository;
+    private final LoanBlockWritePlatformService loanBlockWritePlatformService;
+
+    @PostConstruct
+    public void addEventListeners() {
+        businessEventNotifierService.addPostBusinessEventListener(LoanRescheduledDueAdjustScheduleBusinessEvent.class,
+                new LoanRescheduledDueAdjustScheduleBusinessEventListener());
+    }
+
+    private void updateLoanAndClientWithRestructureBlockDetail(final Loan loan) {
+
+        final CodeValue productType = loan.loanProduct().getProductType();
+
+        if (productType == null || !productType.getLabel().equals(LoanProductType.SUMAS_EMPRESSAS.getCode())) {
+            BlockingReasonSetting setting = loanBlockingReasonRepository.getSingleBlockingReasonSettingByReason(
+                    BlockingReasonSettingEnum.CREDIT_RESTRUCTURE.getDatabaseString(), BlockLevel.CREDIT.toString());
+            loanBlockWritePlatformService.blockLoan(loan.getId(), setting, "Reestructurada", DateUtils.getLocalDateOfTenant());
+        }
+    }
+
+    private final class LoanRescheduledDueAdjustScheduleBusinessEventListener
+            implements BusinessEventListener<LoanRescheduledDueAdjustScheduleBusinessEvent> {
+
+        @Override
+        public void onBusinessEvent(LoanRescheduledDueAdjustScheduleBusinessEvent event) {
+            final Loan loan = event.get();
+            updateLoanAndClientWithRestructureBlockDetail(loan);
+
+        }
+    }
+}
