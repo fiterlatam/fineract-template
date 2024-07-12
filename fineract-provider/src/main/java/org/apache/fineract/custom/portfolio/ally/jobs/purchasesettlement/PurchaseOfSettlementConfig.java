93018fa2c5fdccae484853a2bbae59d19b7d81d1
Tabrez-fiter <tabrez@fiter.io>
2024-07-12 14:45:36 +0800

    SU-182 : Liquidación aliados: liquidación de compras

---
+++ fineract-provider/src/main/java/org/apache/fineract/custom/portfolio/ally/jobs/purchasesettlement/PurchaseOfSettlementConfig.java
@@ -0,0 +1,62 @@
+package org.apache.fineract.custom.portfolio.ally.jobs.purchasesettlement;
+
+import org.apache.fineract.custom.portfolio.ally.domain.AllyCollectionSettlementRepository;
+import org.apache.fineract.custom.portfolio.ally.domain.ClientAllyRepository;
+import org.apache.fineract.custom.portfolio.ally.service.AllyCollectionSettlementReadWritePlatformService;
+import org.apache.fineract.infrastructure.configuration.domain.GlobalConfigurationRepository;
+import org.apache.fineract.infrastructure.jobs.service.JobName;
+import org.apache.fineract.organisation.workingdays.domain.WorkingDaysRepositoryWrapper;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.PlatformTransactionManager;
+
+@Service
+public class PurchaseOfSettlementConfig {
+
+    @Autowired
+    private JobRepository jobRepository;
+    @Autowired
+    private PlatformTransactionManager transactionManager;
+
+    @Autowired
+    private ClientAllyRepository clientAllyRepository;
+
+    @Autowired
+    private WorkingDaysRepositoryWrapper workingDaysRepositoryWrapper;
+
+    @Autowired
+    private AllyCollectionSettlementRepository allyCollectionSettlementRepository;
+
+    @Autowired
+    private AllyCollectionSettlementReadWritePlatformService allyCollectionSettlementReadWritePlatformService;
+
+    @Autowired
+    private GlobalConfigurationRepository globalConfigurationRepository;
+
+    @Bean
+    public Step PurchaseOfSettlementStep() {
+
+        return new StepBuilder(JobName.ALLYPURCHASESETTLEMENT.name(), jobRepository)
+                .tasklet(purchaseOfSettlementTasklet(), transactionManager).build();
+    }
+
+    @Bean
+    public Job PurchaseSettlementJob() {
+
+        return new JobBuilder(JobName.ALLYPURCHASESETTLEMENT.name(), jobRepository).start(PurchaseOfSettlementStep())
+                .incrementer(new RunIdIncrementer()).build();
+    }
+
+    @Bean
+    public PurchaseOfSettlementTasklet purchaseOfSettlementTasklet() {
+        return new PurchaseOfSettlementTasklet(clientAllyRepository, workingDaysRepositoryWrapper, allyCollectionSettlementRepository,
+                allyCollectionSettlementReadWritePlatformService, globalConfigurationRepository);
+    }
+}
