e928290f25b736c696b7afbf6deedaff3f90e6e2
Brian Muhimbura <brianmigel@gmail.com>
2024-08-05 21:04:35 +0300

    Fix/fsf 21 (#990)
    * FSF 22
    
    * FSF 22
    
    * FSF 22

--- fineract-provider/src/main/java/org/apache/fineract/organisation/prequalification/service/PrequalificationChecklistWritePlatformServiceImpl.java
+++ fineract-provider/src/main/java/org/apache/fineract/organisation/prequalification/service/PrequalificationChecklistWritePlatformServiceImpl.java
@@ -261,7 +261,6 @@ public class PrequalificationChecklistWritePlatformServiceImpl implements Prequa
                     mpgm.requested_amount AS requestedAmount, IFNULL(mc.date_of_birth, mpgm.dob) AS dateOfBirth, IFNULL(mc.dpi, mpgm.dpi) AS dpi,
                     mpgm.work_with_puente AS workWithPuente, mcv.code_value As gender, mpgm.is_president AS president, mpgm.buro_check_status as buroCheckStatus, mpgm.agency_bureau_status as agencyBuroStatus,
                     ml.is_topup AS isTopup, ml.id AS loanId, mcvl.code_value as loanCycleCompleted,
-                    ml.is_topup AS isTopup, ml.id AS loanId, mcvl.code_value as loanCycleCompleted,
                     CASE WHEN (? NOT IN (3,7,4,5)) AND (COALESCE(mc.loan_cycle, 0) >= 3) THEN 'RECURRING'
                     WHEN (? IN (4,5)) AND (COALESCE(mc.loan_cycle, 0) >= 1) THEN 'RECURRING'
                     ELSE 'NEW' END as clientCategorization,
@@ -278,7 +277,7 @@ public class PrequalificationChecklistWritePlatformServiceImpl implements Prequa
                     LEFT JOIN m_prequalification_group mpg ON mpg.id = mpgm.group_id
                     LEFT JOIN m_loan ml ON ml.client_id = mc.id AND ml.loan_status_id = 100 AND ml.prequalification_id = mpg.id
                     LEFT JOIN m_loan_additionals_group mlad ON mlad.loan_id = ml.id
-                    LEFT JOIN m_code_value mcvl ON mcv.id = mlad.loan_cycle_completed
+                    LEFT JOIN m_code_value mcvl ON mcvl.id = mlad.loan_cycle_completed
                     WHERE mpg.id = ? GROUP BY mc.id
                     """;
         }
@@ -440,7 +439,7 @@ public class PrequalificationChecklistWritePlatformServiceImpl implements Prequa
 
         final Map<String, String> reportParams = new HashMap<>();
         reportParams.put("${loanProductId}", productId);
-        reportParams.put("${categorization}", clientData.getCategorization());
+        reportParams.put("${categorization}", clientData.getRecreditCategorization());
         reportParams.put("${investmentPlan}", Long.toString(investmentPlanCount));
         reportParams.put("${photographs}", Long.toString(photographsCount));
         reportParams.put("${requestedAmount}", clientData.getRequestedAmount().toPlainString());
