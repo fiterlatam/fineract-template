5ab39965e224ec9e54f11466a56756767c4c8613
Faheem Ahmad <faheemahmad@sanmar.com>
2024-08-09 03:49:18 +0500

    SU-273 spotless fix applied

--- fineract-loan/src/main/java/org/apache/fineract/portfolio/loanaccount/domain/LoanCharge.java
+++ fineract-loan/src/main/java/org/apache/fineract/portfolio/loanaccount/domain/LoanCharge.java
@@ -224,7 +224,8 @@ public class LoanCharge extends AbstractAuditableWithUTCDateTimeCustom {
                 final RoundingMode roundingMode = RoundingMode.HALF_UP;
                 final MathContext mc = MoneyHelper.getMathContext();
                 // Get one day of interest
-                this.percentage = chargeAmount.divide(BigDecimal.valueOf(365), mc).setScale(5, roundingMode).multiply(BigDecimal.valueOf(100L));
+                this.percentage = chargeAmount.divide(BigDecimal.valueOf(365), mc).setScale(5, roundingMode)
+                        .multiply(BigDecimal.valueOf(100L));
 
                 this.amountPercentageAppliedTo = amountPercentageAppliedTo;
                 if (loanCharge.compareTo(BigDecimal.ZERO) == 0) {
