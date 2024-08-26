578f453eb28935aee8fcdf83228281be28d26af4
tabrez-fiter <tabrez@fiter.io>
2024-08-26 16:28:43 +0800

    SU-190 : Compensación aliados: pantalla

--- fineract-provider/src/main/java/org/apache/fineract/custom/portfolio/ally/service/AllyCompensationReadWritePlatformService.java
+++ fineract-provider/src/main/java/org/apache/fineract/custom/portfolio/ally/service/AllyCompensationReadWritePlatformService.java
@@ -1,19 +1,19 @@
 package org.apache.fineract.custom.portfolio.ally.service;
 
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
 import org.apache.fineract.custom.portfolio.ally.data.AllySettlementCompansationCollectionData;
 import org.apache.fineract.custom.portfolio.ally.data.ClientAllySettlementData;
 import org.apache.fineract.custom.portfolio.ally.domain.AllyCompensation;
 import org.apache.fineract.infrastructure.core.api.JsonCommand;
 import org.apache.fineract.infrastructure.core.data.CommandProcessingResult;
 
-import java.util.List;
-import java.util.Optional;
-
 public interface AllyCompensationReadWritePlatformService {
 
     List<AllySettlementCompansationCollectionData> getListCompensationSettlement();
 
-    Optional<AllySettlementCompansationCollectionData> getCompensationSettlementByNit(String nit, String startDate, String endDate);
+    Optional<AllySettlementCompansationCollectionData> getCompensationSettlementByNit(String nit, LocalDate startDate, LocalDate endDate);
 
     List<ClientAllySettlementData> getClientAllySettlement();
 
