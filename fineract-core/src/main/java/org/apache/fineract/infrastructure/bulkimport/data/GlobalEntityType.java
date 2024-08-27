e22575eb78d1fa95dc184eac1a4fee512d15399f
Faheem Ahmad <faheemahmad@sanmar.com>
2024-08-27 17:08:27 +0500

    SU-169 spotless check applied

--- fineract-core/src/main/java/org/apache/fineract/infrastructure/bulkimport/data/GlobalEntityType.java
+++ fineract-core/src/main/java/org/apache/fineract/infrastructure/bulkimport/data/GlobalEntityType.java
@@ -63,8 +63,8 @@ public enum GlobalEntityType {
     CLIENT_ALLY_POINTS_OF_SALES(1001, "clients.allies.points.of.sales"), //
     CLIENT_BLOCK(37, "clientblock"), //
     CLIENT_VIP(38, "clientvip"), //
-    COMMERCE_POINT_OF_SALE(39, "commercepointofsale"), LOAN_WRITE_OFFS(40, "loanwriteoffs"),
-    SALES_OF_INSURANCE_OR_ASSISTANCE(41, "sales.of.insurance.or.assistance");
+    COMMERCE_POINT_OF_SALE(39, "commercepointofsale"), LOAN_WRITE_OFFS(40, "loanwriteoffs"), SALES_OF_INSURANCE_OR_ASSISTANCE(41,
+            "sales.of.insurance.or.assistance");
 
     private final Integer value;
     private final String code;
