d8f4d5445aea770ce6a289e2113576d67076dac1
Faheem Ahmad <julius@fiter.io>
2024-09-05 17:23:31 +0500

    Su 173 (#1110)
    SU-173

--- fineract-provider/src/main/java/org/apache/fineract/portfolio/loanaccount/handler/CancelInsuranceDueToBadSaleCommandHandler.java
+++ fineract-provider/src/main/java/org/apache/fineract/portfolio/loanaccount/handler/CancelInsuranceDueToBadSaleCommandHandler.java
@@ -38,6 +38,6 @@ public class CancelInsuranceDueToBadSaleCommandHandler implements NewCommandSour
     @Override
     public CommandProcessingResult processCommand(final JsonCommand command) {
 
-        return this.writePlatformService.cancelInsurance(command, true);
+        return this.writePlatformService.cancelInsurance(command, false);
     }
 }
