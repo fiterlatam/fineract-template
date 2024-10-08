8e6560a713d9ee9a7b4337021377565d30ba0f3b
tabrez-fiter <tabrez@fiter.io>
2024-10-08 16:29:29 +0800

    SU-322 :REDIFERIDOS / Seguros Obligatorios y voluntarios

--- fineract-loan/src/main/java/org/apache/fineract/portfolio/loanaccount/domain/Loan.java
+++ fineract-loan/src/main/java/org/apache/fineract/portfolio/loanaccount/domain/Loan.java
@@ -3605,10 +3605,6 @@ public class Loan extends AbstractAuditableWithUTCDateTimeCustom {
             }
         }
 
-        // if(this.getId() != 1L){
-        // throw new GeneralPlatformDomainRuleException("error.msg.loan.id.not.1", "Loan id should be 1");
-        // }
-
         return changedTransactionDetail;
     }
 
