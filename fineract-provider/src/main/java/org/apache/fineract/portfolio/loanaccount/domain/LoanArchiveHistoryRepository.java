ffb0bbc8ededf671a463cda53dbec919c21b18e2
tabrez-fiter <tabrez@fiter.io>
2024-10-16 11:55:31 +0800

    SU-384 : Archivo de cartera - Daily Portfolio report generation

---
+++ fineract-provider/src/main/java/org/apache/fineract/portfolio/loanaccount/domain/LoanArchiveHistoryRepository.java
@@ -0,0 +1,17 @@
+package org.apache.fineract.portfolio.loanaccount.domain;
+
+import java.util.List;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+public interface LoanArchiveHistoryRepository
+        extends JpaRepository<LoanArchiveHistory, Long>, JpaSpecificationExecutor<LoanArchiveHistory> {
+
+    @Query("SELECT lah FROM LoanArchiveHistory lah WHERE lah.numeroObligacion NOT IN :numeroObligacionList")
+    List<LoanArchiveHistory> findByNumeroObligacionNotIn(@Param("numeroObligacionList") List<String> numeroObligacionList);
+
+    Optional<LoanArchiveHistory> findByTitle(String title);
+}
