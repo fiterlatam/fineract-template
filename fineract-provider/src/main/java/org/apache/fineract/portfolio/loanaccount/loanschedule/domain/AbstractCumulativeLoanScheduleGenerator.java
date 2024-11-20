4fb534870411b96596b20cbfe7ef90948c8b53cd
Faheem Ahmad <julius@fiter.io>
2024-11-21 00:37:31 +0500

    SU-320 (#1304)
    * SU-320 SU-443

--- fineract-provider/src/main/java/org/apache/fineract/portfolio/loanaccount/loanschedule/domain/AbstractCumulativeLoanScheduleGenerator.java
+++ fineract-provider/src/main/java/org/apache/fineract/portfolio/loanaccount/loanschedule/domain/AbstractCumulativeLoanScheduleGenerator.java
@@ -3071,7 +3071,9 @@ public abstract class AbstractCumulativeLoanScheduleGenerator implements LoanSch
                         }
                     }
                     // Extra amount paid in advance
-                    if (outstandingBalance.isLessThanZero()) {
+                    if (outstandingBalance.isZero() || outstandingBalance.isLessThanZero()) {
+                        // Check outstandingBalance.isZero() in case customer pays exact amount equal to outstanding
+                        // balance
                         // If overpaid in advance then add the last installment to keep track of advance payment amount
                         outstandingBalanceAsPerRest = outstandingBalance.zero();
                         outstandingBalance = outstandingBalance.zero();
@@ -3283,7 +3285,7 @@ public abstract class AbstractCumulativeLoanScheduleGenerator implements LoanSch
                     .plus(totalPenaltyChargesCharged);
 
             // for partial schedule generation
-            if (!newRepaymentScheduleInstallments.isEmpty() && totalCumulativeInterest.isGreaterThanZero()) {
+            if (!newRepaymentScheduleInstallments.isEmpty()) {
                 Money totalOutstandingInterestPaymentDueToGrace = Money.zero(currency);
                 loanScheduleParams = LoanScheduleParams.createLoanScheduleParamsForPartialUpdate(periodNumber, instalmentNumber,
                         loanTermInDays, periodStartDate, actualRepaymentDate, totalCumulativePrincipal, totalCumulativeInterest,
@@ -3362,7 +3364,7 @@ public abstract class AbstractCumulativeLoanScheduleGenerator implements LoanSch
             MonetaryCurrency currency) {
         List<LoanRepaymentScheduleInstallment> newRepaymentScheduleInstallments = new ArrayList<>();
         for (LoanRepaymentScheduleInstallment installment : repaymentScheduleInstallments) {
-            if (DateUtils.isBefore(installment.getFromDate(), rescheduleFrom)) {
+            if (DateUtils.isOnOrBefore(installment.getFromDate(), rescheduleFrom)) {
                 newRepaymentScheduleInstallments.add(installment);
             } else {
                 // Check if there is any installment having advance payment then add the installment to calculate
