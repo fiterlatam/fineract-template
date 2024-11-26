9e2d3b7ec1707283081175a809f121fbe4d1ab46
fiter-julius-oketayot <140818859+fiter-julius-oketayot@users.noreply.github.com>
2024-11-26 15:00:02 +0300

    Feature/SU-200: Factura electrónica - Intercrédito-Su+pay, Su+ vehículo, su+ Empresa (#1311)

--- fineract-core/src/main/java/org/apache/fineract/infrastructure/configuration/domain/ConfigurationDomainService.java
+++ fineract-core/src/main/java/org/apache/fineract/infrastructure/configuration/domain/ConfigurationDomainService.java
@@ -38,6 +38,8 @@ public interface ConfigurationDomainService {
 
     boolean allowTransactionsOnNonWorkingDayEnabled();
 
+    boolean enableMonthlyInvoiceGenerationOnJobTrigger();
+
     boolean isConstraintApproachEnabledForDatatables();
 
     boolean isEhcacheEnabled();
