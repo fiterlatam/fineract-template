841e4ff36894344df61400f0f7fe5496a8967ddd
tabrez-fiter <tabrez@fiter.io>
2024-12-08 14:11:02 +0800

    SU-470: Mail Configuration - Remove authentification

--- fineract-provider/src/main/java/org/apache/fineract/infrastructure/reportmailingjob/service/ReportMailingJobEmailServiceImpl.java
+++ fineract-provider/src/main/java/org/apache/fineract/infrastructure/reportmailingjob/service/ReportMailingJobEmailServiceImpl.java
@@ -91,7 +91,7 @@ public class ReportMailingJobEmailServiceImpl implements ReportMailingJobEmailSe
     private Properties getJavaMailProperties(Collection<ReportMailingJobConfigurationData> reportMailingJobConfigurationDataCollection) {
         Properties properties = new Properties();
 
-        properties.setProperty("mail.smtp.auth", "true");
+        properties.setProperty("mail.smtp.auth", "false");
         properties.setProperty("mail.smtp.starttls.enable", "true");
         properties.setProperty("mail.smtp.ssl.trust", this.getGmailSmtpServer(reportMailingJobConfigurationDataCollection));
 
