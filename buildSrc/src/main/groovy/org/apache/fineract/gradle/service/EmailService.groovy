d69cf14a4392282fd779d9fd5bea15ee71ca59c5
tabrez-fiter <tabrez@fiter.io>
2024-12-09 17:07:49 +0800

    SU-470 : Mail Configuration - Remove authentification

--- buildSrc/src/main/groovy/org/apache/fineract/gradle/service/EmailService.groovy
+++ buildSrc/src/main/groovy/org/apache/fineract/gradle/service/EmailService.groovy
@@ -43,7 +43,7 @@ class EmailService {
         this.properties.put("mail.smtp.auth", "false")
         this.properties.put("mail.smtp.starttls.enable", config.tls.toString())
         if(config.ssl) {
-            this.properties.put("mail.smtp.`port", "465")
+            this.properties.put("mail.smtp.port", "465")
             this.properties.put("mail.smtp.socketFactory.port", "465");
             this.properties.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
 
