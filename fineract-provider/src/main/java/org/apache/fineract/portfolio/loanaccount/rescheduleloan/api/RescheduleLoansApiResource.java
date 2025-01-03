047e9734c2bcaebe097bb4678ad26da2da38f5d9
tabrez-fiter <tabrez@fiter.io>
2025-01-04 02:09:42 +0800

    SU-453 : Sprint 14 - 15 (5) Rediferido

--- fineract-provider/src/main/java/org/apache/fineract/portfolio/loanaccount/rescheduleloan/api/RescheduleLoansApiResource.java
+++ fineract-provider/src/main/java/org/apache/fineract/portfolio/loanaccount/rescheduleloan/api/RescheduleLoansApiResource.java
@@ -89,7 +89,7 @@ public class RescheduleLoansApiResource {
     @Operation(summary = "Retrieve all reschedule loan reasons", description = "Retrieve all reschedule loan reasons as a template")
     @ApiResponses({
             @ApiResponse(responseCode = "200", description = "OK", content = @Content(schema = @Schema(implementation = RescheduleLoansApiResourceSwagger.GetRescheduleReasonsTemplateResponse.class))) })
-    public String retrieveTemplate(@Context final UriInfo uriInfo, @QueryParam("loanId") Long loanId,
+    public String retrieveTemplatetillDate(@Context final UriInfo uriInfo, @QueryParam("loanId") Long loanId,
             @QueryParam("tillDate") String tillDate) {
         this.platformSecurityContext.authenticatedUser().validateHasReadPermission(RescheduleLoansApiConstants.ENTITY_NAME);
         final ApiRequestJsonSerializationSettings settings = this.apiRequestParameterHelper.process(uriInfo.getQueryParameters());
