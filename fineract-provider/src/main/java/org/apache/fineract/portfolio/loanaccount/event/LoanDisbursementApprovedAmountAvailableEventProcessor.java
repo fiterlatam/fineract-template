180b4d6c4f707bf90fb85c1d689882c04b09c4f8
Farooq <farooq@fiter.io>
2025-03-04 21:04:57 +0100

    fix sonar issues

--- fineract-provider/src/main/java/org/apache/fineract/portfolio/loanaccount/event/LoanDisbursementApprovedAmountAvailableEventProcessor.java
+++ fineract-provider/src/main/java/org/apache/fineract/portfolio/loanaccount/event/LoanDisbursementApprovedAmountAvailableEventProcessor.java
@@ -29,9 +29,6 @@ import org.apache.fineract.commands.event.BaseCustomWebhookEventProcessorImpl;
 import org.apache.fineract.custom.infrastructure.dataqueries.data.InformacionAdicionalDatatableData;
 import org.apache.fineract.infrastructure.core.api.JsonCommand;
 import org.apache.fineract.infrastructure.core.data.CommandProcessingResult;
-import org.apache.fineract.infrastructure.core.data.EnumOptionData;
-import org.apache.fineract.portfolio.client.data.ClientData;
-import org.apache.fineract.portfolio.client.service.ClientReadPlatformService;
 import org.apache.fineract.portfolio.loanaccount.domain.Loan;
 import org.apache.fineract.portfolio.loanaccount.domain.LoanRepositoryWrapper;
 import org.apache.fineract.portfolio.loanproduct.domain.LoanProductType;
@@ -47,7 +44,6 @@ public class LoanDisbursementApprovedAmountAvailableEventProcessor extends BaseC
     public static final String PRODUCT_NAME_PARAM = "productName";
     public static final String LOAN_AMOUNT_PARAM = "loanAmount";
 
-    private final ClientReadPlatformService clientReadPlatformService;
     private final LoanRepositoryWrapper loanRepositoryWrapper;
     private final LoanDisbursementCreditoRotativoEventProcessor loanDisbursementCreditoRotativoEventProcessor;
 
@@ -86,8 +82,8 @@ public class LoanDisbursementApprovedAmountAvailableEventProcessor extends BaseC
     private void getRequestBody(CommandProcessingResult result, Map<String, Object> requestBody, Loan loan,
             InformacionAdicionalDatatableData informacionAdicional) {
 
-    // If Monto Disponible is checked, send the request body
-    if (Boolean.TRUE.equals(informacionAdicional.getMontoDisponible())) {
+        // If Monto Disponible is checked, send the request body
+        if (Boolean.TRUE.equals(informacionAdicional.getMontoDisponible())) {
             requestBody.put(LOAN_ID_PARAM, result.getLoanId());
 
             requestBody.put(PRODUCT_NAME_PARAM, loan.getLoanProduct().getName());
