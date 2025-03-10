0380598e60f6dbc5a81b77a98afe87f06ad02e8a
Frank Nkuyahaga <hagafrank@gmail.com>
2025-03-10 12:28:59 +0300

    SU-631: Trigger electronic credit note process from monthly invoice job

--- fineract-provider/src/main/java/org/apache/fineract/portfolio/loanaccount/service/LoanCreditNoteReadService.java
+++ fineract-provider/src/main/java/org/apache/fineract/portfolio/loanaccount/service/LoanCreditNoteReadService.java
@@ -1,6 +1,7 @@
 package org.apache.fineract.portfolio.loanaccount.service;
 
 import java.util.Collection;
+import java.util.List;
 import org.apache.fineract.portfolio.loanaccount.data.LoanCreditNoteData;
 
 public interface LoanCreditNoteReadService {
@@ -8,4 +9,6 @@ public interface LoanCreditNoteReadService {
     Collection<LoanCreditNoteData> retrieveAllCreditNotesForLoan(Long loanId);
 
     LoanCreditNoteData retrieveCreditNoteForLoan(Long loanId, Long creditNoteId);
+
+    List<Long> retrieveCreditNoteIdsForInvoiceProcessing();
 }
