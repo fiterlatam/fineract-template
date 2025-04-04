c604543a1aa1a25146a258b46bfe5d01334186cc
Frank Nkuyahaga <hagafrank@gmail.com>
2025-04-04 12:12:22 +0300

    SU-651: Spotless fix

--- fineract-loan/src/main/java/org/apache/fineract/portfolio/loanaccount/domain/Loan.java
+++ fineract-loan/src/main/java/org/apache/fineract/portfolio/loanaccount/domain/Loan.java
@@ -48,7 +48,6 @@ import java.time.temporal.ChronoUnit;
 import java.util.*;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
-
 import lombok.Getter;
 import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
