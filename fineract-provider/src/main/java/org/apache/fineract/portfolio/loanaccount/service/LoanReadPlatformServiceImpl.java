e8429d13d922c92dbd66dfe9209a586a4303df9c
Farooq <farooq@fiter.io>
2025-06-17 23:57:32 +0100

    [EA-368] Installment schedule not updated after disbursement made post cut-off date

--- fineract-provider/src/main/java/org/apache/fineract/portfolio/loanaccount/service/LoanReadPlatformServiceImpl.java
+++ fineract-provider/src/main/java/org/apache/fineract/portfolio/loanaccount/service/LoanReadPlatformServiceImpl.java
@@ -1618,7 +1618,6 @@ public class LoanReadPlatformServiceImpl implements LoanReadPlatformService, Loa
             BigDecimal disbursedAmount = BigDecimal.ZERO;
             for (final DisbursementData data : disbursementData) {
                 boolean isDueForDisbursement = data.isDueForDisbursement(loanScheduleType, fromDate, dueDate);
-                boolean isFirstDisbursement = findPositionInCollection(disbursementData, data) == 0;
                 boolean isDisbursementAllowed = ((fromDate.equals(this.disbursement.disbursementDate())
                         && data.disbursementDate().equals(fromDate))
                         || (fromDate.equals(dueDate) && data.disbursementDate().equals(fromDate))
