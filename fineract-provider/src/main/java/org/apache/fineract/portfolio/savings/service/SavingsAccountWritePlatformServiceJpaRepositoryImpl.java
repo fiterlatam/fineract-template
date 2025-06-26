b734d177a7f17c245c7bf0ca2ac7b6733da3ca35
brian <brianmigel@gmail.com>
2025-06-26 20:38:36 +0300

    fix/FSF-181

--- fineract-provider/src/main/java/org/apache/fineract/portfolio/savings/service/SavingsAccountWritePlatformServiceJpaRepositoryImpl.java
+++ fineract-provider/src/main/java/org/apache/fineract/portfolio/savings/service/SavingsAccountWritePlatformServiceJpaRepositoryImpl.java
@@ -2300,9 +2300,9 @@ public class SavingsAccountWritePlatformServiceJpaRepositoryImpl implements Savi
     }
 
     protected CommandProcessingResult rebalanceAllSavingsAccounts() {
-        String accountsQuery = "select savings_account_id from m_savings_account_transaction where running_balance_derived <0 GROUP BY savings_account_id";
+        String accountsQuery = "select savings_account_id from m_savings_account_transaction where running_balance_derived <0 GROUP BY savings_account_id LIMIT 1000";
         List<Long> accountIds = this.jdbcTemplate.query(accountsQuery, (rs, rowNum) -> rs.getLong("savings_account_id"));
-        accountIds.parallelStream().forEach(accountId -> {
+        accountIds.forEach(accountId -> {
             try {
                 LOG.info("Going to Rebalance account with ID: {}", accountId);
                 this.rebalanceSingleAccount(accountId);
