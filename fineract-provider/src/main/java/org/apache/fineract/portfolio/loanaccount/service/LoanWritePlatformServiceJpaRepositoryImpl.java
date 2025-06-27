6898e0f306857e40edfe8d039f7349cdab92574a
fiter-julius-oketayot <140818859+fiter-julius-oketayot@users.noreply.github.com>
2025-06-27 17:53:55 +0300

    Bug/SU-713:Incorrect Billing Alignment for Suspended Portfolio  Invoice Generated for Already-Billed Amounts (#1954)

--- fineract-provider/src/main/java/org/apache/fineract/portfolio/loanaccount/service/LoanWritePlatformServiceJpaRepositoryImpl.java
+++ fineract-provider/src/main/java/org/apache/fineract/portfolio/loanaccount/service/LoanWritePlatformServiceJpaRepositoryImpl.java
@@ -4396,7 +4396,6 @@ public class LoanWritePlatformServiceJpaRepositoryImpl implements LoanWritePlatf
                 final Set<LoanTransactionData> invoicedByAccrualTransactionDataSet = new HashSet<>();
                 final Set<LoanTransaction> invoicedByAccrualTransactionSet = new HashSet<>();
                 final List<LoanTransaction> accrualTransactions = loan.retrieveListOfAccrualTransactions().stream()
-                        .filter(LoanTransaction::isInvoicedNotGeneratedByJob)
                         .filter(ltx -> Objects.isNull(ltx.getInvoicedByTransactionId()) || ltx.isPartiallyInvoiced()).toList();
                 for (final LoanTransaction accrualTransaction : accrualTransactions) {
                     final boolean occurredOnSuspendedAccount = accrualTransaction.hasOccurredOnSuspendedAccount();
