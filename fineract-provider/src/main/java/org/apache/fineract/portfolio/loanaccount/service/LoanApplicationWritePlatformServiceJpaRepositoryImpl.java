0ba7f90796f3d9dc7607f7cc47ef6ca528111d5a
dstanca <dstanca@vwodonto.com.br>
2025-12-11 11:40:08 -0300

    feature/EAO-441 - Migracion - Cargo Manual  MiPyme

--- fineract-provider/src/main/java/org/apache/fineract/portfolio/loanaccount/service/LoanApplicationWritePlatformServiceJpaRepositoryImpl.java
+++ fineract-provider/src/main/java/org/apache/fineract/portfolio/loanaccount/service/LoanApplicationWritePlatformServiceJpaRepositoryImpl.java
@@ -2054,7 +2054,7 @@ public class LoanApplicationWritePlatformServiceJpaRepositoryImpl implements Loa
 
     private void validateMicrocreditoProductCharges(Loan loan) {
 
-        if (loan.getLoanProduct().getName().toLowerCase().contains(STRING_PRODUCT_MICROCREDITO.toLowerCase())) {
+        if (!loan.isMigratedLoan() && loan.getLoanProduct().getName().toLowerCase().contains(STRING_PRODUCT_MICROCREDITO.toLowerCase())) {
 
             final String filterCriteria = constructMiPymeChargeFilterCriteria(loan);
 
