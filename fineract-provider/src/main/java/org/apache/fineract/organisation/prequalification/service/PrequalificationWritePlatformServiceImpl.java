8cb926b72c40685ab4c7e3c26df62db31a9a6de7
Andrés M. Ocampo Arenas <andresmauricioocampo2@gmail.com>
2025-12-15 08:20:32 -0500

    status fix for comite D and rejecteds

--- fineract-provider/src/main/java/org/apache/fineract/organisation/prequalification/service/PrequalificationWritePlatformServiceImpl.java
+++ fineract-provider/src/main/java/org/apache/fineract/organisation/prequalification/service/PrequalificationWritePlatformServiceImpl.java
@@ -1081,7 +1081,7 @@ public class PrequalificationWritePlatformServiceImpl implements Prequalificatio
             final boolean isApproved = PrequalificationStatus.COMPLETED.equals(prequalificationStatus)
                     && (memberPrequalificationData.getIsSelected() || prequalificationGroup.isPrequalificationTypeIndividual());
             final boolean isRejected = PrequalificationStatus.REJECTED.equals(prequalificationStatus)
-                    || (!memberPrequalificationData.getIsSelected() && PrequalificationStatus.COMPLETED.equals(prequalificationStatus)
+                    || (memberPrequalificationData.getIsSelected() != null && !memberPrequalificationData.getIsSelected() && PrequalificationStatus.COMPLETED.equals(prequalificationStatus)
                             && prequalificationGroup.isPrequalificationTypeGroup());
             final BigDecimal approvedLoanAmount = prequalificationGroupMember.getApprovedAmount();
             final String dpi = prequalificationGroupMember.getDpi();
@@ -1268,6 +1268,8 @@ public class PrequalificationWritePlatformServiceImpl implements Prequalificatio
             status = PrequalificationStatus.COMPLETED;
         } else if (action.equalsIgnoreCase("pending")) {
             status = PrequalificationStatus.PENDING;
+        } else if (action.equalsIgnoreCase("reject")) {
+            status = PrequalificationStatus.REJECTED;
         }
         return status;
     }
