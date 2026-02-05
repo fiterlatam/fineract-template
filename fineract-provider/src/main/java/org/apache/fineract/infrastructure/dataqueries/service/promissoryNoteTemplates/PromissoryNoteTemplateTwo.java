61cf0e5530ad8c6d96519a106e3b6db50c6a79d3
brian <brianmigel@gmail.com>
2026-02-05 14:18:31 +0300

    Cancel pending renegotiations after approval in PrequalificationWritePlatformServiceImpl

--- fineract-provider/src/main/java/org/apache/fineract/infrastructure/dataqueries/service/promissoryNoteTemplates/PromissoryNoteTemplateTwo.java
+++ fineract-provider/src/main/java/org/apache/fineract/infrastructure/dataqueries/service/promissoryNoteTemplates/PromissoryNoteTemplateTwo.java
@@ -200,9 +200,10 @@ public class PromissoryNoteTemplateTwo {
 
                             Lugar y fecha de emisión: Municipio y Departamento de %s, %s de %s, del año dos mil %s.
                             """,
-                    clientName, clientDpiText, clientDpiNumber, clientAddress, creditAmountText, creditPurpose, creditDetail, termText, disbursementDate,
-                    secondTermText, numberEqualsQuotas + " de " + quotaAmount, numberLastQuota, lastQuotaAmount, paymentDay,
-                    interestRateText, department, date.getDayOfMonth(), date.getMonth().getDisplayName(TextStyle.FULL, new Locale("es")),
+                    clientName, clientDpiText, clientDpiNumber, clientAddress, creditAmountText, creditPurpose, creditDetail, termText,
+                    disbursementDate, secondTermText, numberEqualsQuotas + " de " + quotaAmount, numberLastQuota, lastQuotaAmount,
+                    paymentDay, interestRateText, department, date.getDayOfMonth(),
+                    date.getMonth().getDisplayName(TextStyle.FULL, new Locale("es")),
                     DateUtils.numberToLetters(date.getYear() - 2000).toLowerCase());
             Paragraph body = new Paragraph(bodyText, normalFont);
             body.setAlignment(Element.ALIGN_JUSTIFIED);
