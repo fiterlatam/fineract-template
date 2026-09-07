9e474b009b0c5946987e759668747a13f245d2f4
brian <brianmigel@gmail.com>
2026-09-07 11:01:12 +0300

    https://fiterio.odoo.com/odoo/action-197/15/tasks/998

--- fineract-provider/src/main/java/org/apache/fineract/organisation/prequalification/service/PrequalificationWritePlatformServiceImpl.java
+++ fineract-provider/src/main/java/org/apache/fineract/organisation/prequalification/service/PrequalificationWritePlatformServiceImpl.java
@@ -907,12 +907,15 @@ public class PrequalificationWritePlatformServiceImpl implements Prequalificatio
          * Monto > 250.000 → Comité A prequalificationGroup.updateStatus(
          * PrequalificationStatus.PRE_COMMITTEE_A_PENDING_APPROVAL ); } }
          */
-        if (action.equals("approveRenegotiation") && !nextPhase) {
+        if ((action.equals("approvepreviouscommitee") || action.equals("approveRenegotiation")) && !nextPhase) {
 
-            PrequalificationStatus lastStatus = PrequalificationStatus
-                    .fromInt(prequalificationData.getLastPrequalificationStatus().getId().intValue());
+            if (action.equals("approveRenegotiation") && !nextPhase){
+                PrequalificationStatus lastStatus = PrequalificationStatus
+                        .fromInt(prequalificationData.getLastPrequalificationStatus().getId().intValue());
 
-            prequalificationGroup.updateStatus(lastStatus);
+                prequalificationGroup.updateStatus(lastStatus);
+            }
+
 
         } else {
             if (!nextPhase) {
