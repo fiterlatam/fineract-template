e9a619126f053de3a8e8a5d902fdeca856e0a1b7
brian <brianmigel@gmail.com>
2026-09-17 23:49:23 +0300

    Performance Enhancements

--- fineract-provider/src/main/java/org/apache/fineract/organisation/prequalification/service/PrequalificationWritePlatformServiceImpl.java
+++ fineract-provider/src/main/java/org/apache/fineract/organisation/prequalification/service/PrequalificationWritePlatformServiceImpl.java
@@ -905,11 +905,16 @@ public class PrequalificationWritePlatformServiceImpl implements Prequalificatio
          * PrequalificationStatus.PRE_COMMITTEE_A_PENDING_APPROVAL ); } }
          */
         if ((action.equals("approvepreviouscommitee") || action.equals("approveRenegotiation")) && !nextPhase) {
-
             PrequalificationStatus lastStatus = PrequalificationStatus
                     .fromInt(prequalificationData.getLastPrequalificationStatus().getId().intValue());
 
-            prequalificationGroup.updateStatus(lastStatus);
+            if (action.equals("approveRenegotiation") && !nextPhase) {
+
+                prequalificationGroup.updateStatus(lastStatus);
+            }
+            if (action.equals("approvepreviouscommitee")) {
+                fromStatus = lastStatus.getValue();
+            }
 
         } else {
             if (!nextPhase) {
